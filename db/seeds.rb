9845e722c6bc4fa83b4706e1dbe0d07c30113d0a
Christian Jaroljmek <christian@jaroljmek.com>
2022-01-20 20:26:05 +0000

    Done Routes Controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning"
+Restaurant.destroy_all
+puts "database clean"
+
+puts "Creating Restaurants"
+20.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  Review.create(
+    content: ["In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.", "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry?
+"].sample,
+    rating: rand(1...5),
+    restaurant: restaurant
+  )
+end
