5ce3706596a6577171a4171cfa80b98072161e39
Christian Jaroljmek <christian@jaroljmek.com>
2022-01-22 11:06:32 +0000

    Yelp nearly finishded

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review.permit(:content, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
